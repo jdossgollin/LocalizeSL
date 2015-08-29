a0881233e0d36d5cfc3cc25606763eaacf28aebf
Bob Kopp <robert-dot-kopp-at-rutgers-dot-edu>
2015-08-29 19:29:42 -0400

    Added WriteTableSLRHeightExceedanceTiming.m.

---
+++ MFILES/WriteTableSLRHeightExceedanceTiming.m
@@ -0,0 +1,42 @@
+function WriteTableSLRHeightExceedanceTiming(sampslocrise,heights,siteids,sitenames,targyears,scens,oneway,fileprefix) 
+
+
+% WriteTableSLRHeightExceedanceTiming(sampslocrise,heights,siteids,sitenames,targyears,scens,oneway,[fileprefix])
+%
+% Write table showing the probability of when heights (in cm) are exceeded.
+%
+% sampslocrise, siteids, sitenames, targyears, scens are outputted by LocalizeStoredProjections.
+%
+% If oneway is set to 1, then any samples with sea-level falls
+% will be treated as though they stayed at their maximum value.
+%
+% Last updated by Robert Kopp, robert-dot-kopp-at-rutgers-dot-edu, Sat Aug 29 19:05:03 EDT 2015
+
+defval('fileprefix','LSLheights_');
+defval('heights',10:10:500);
+defval('oneway',0);
+
+
+for jjj=1:length(siteids)
+    fid=fopen([fileprefix num2str(siteids(jjj)) '.tsv'],'w');
+    for kkk=1:length(scens)
+        Nsamps=size(sampslocrise{jjj,kkk},1);
+        fprintf(fid,[sitenames{jjj} ' [' num2str(siteids(jjj)) '] -- ' scens{kkk} ' (cm above 2000 CE level)\n']);
+        fprintf(fid,'\t %0.1f',heights);
+        fprintf(fid,'\n');
+        for ttt=1:length(targyears)
+            fprintf(fid,'%0.0f',targyears(ttt));
+            if oneway
+                workslr = max(sampslocrise{jjj,kkk}(:,1:ttt)/10,[],2);
+            else
+                workslr = sampslocrise{jjj,kkk}(:,ttt)/10;
+            end
+            u=bsxfun(@gt,workslr,heights(:)');
+            u=sum(u,1)/Nsamps;
+            fprintf(fid,'\t%0.3f',u);
+            fprintf(fid,'\n');
+        end
+        fprintf(fid,'\n\n');        
+    end
+    fclose(fid);
+end
