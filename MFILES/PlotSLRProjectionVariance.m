2922d1f10f88f41f1ba31b632b12fad9dd830488
Bob Kopp <robert-dot-kopp-at-rutgers-dot-edu>
2018-01-02 18:06:18 -0500

    ability to pool across PDFs

--- MFILES/PlotSLRProjectionVariance.m
+++ MFILES/PlotSLRProjectionVariance.m
@@ -1,6 +1,6 @@
-function [hp,vars,fvars,hlg]=PlotSLRProjectionVariance(sampsloccomponents,targyears,cols,limyrs,sitesel,scensel,dopooledscens,subcomp,labls,colrs,dopanels)
+function [hp,vars,fvars,hlg]=PlotSLRProjectionVariance(sampsloccomponents,targyears,cols,limyrs,sitesel,scensel,dopooledscens,subcomp,labls,colrs,dopanels,dopooledPDFs)
 
-% PlotSLRProjectionVariance(sampsloccomponents,targyears,cols,[limyrs],[sitesel],[scensel],[dopooledscens],[subcomp],[labls],[colrs],[dopanels])
+% PlotSLRProjectionVariance(sampsloccomponents,targyears,cols,[limyrs],[sitesel],[scensel],[dopooledscens],[subcomp],[labls],[colrs],[dopanels],[dopooledPDFs])
 %
 % Plot variance decomposition of sea-level rise projections.
 %
@@ -9,18 +9,24 @@ function [hp,vars,fvars,hlg]=PlotSLRProjectionVariance(sampsloccomponents,targye
 % sitesel is the row id of the site of interest in sampslocrise (default: 1)
 % scensel is the sequential id of scenario of interest (default: 1)
 %
-% Last updated by Robert Kopp, robert-dot-kopp-at-rutgers-dot-edu, Sat Jan 14 17:16:36 EST 2017
+% Last updated by Robert Kopp, robert-dot-kopp-at-rutgers-dot-edu, 2018-01-02 17:01:06 -0500
 
 % variance plots
 
-defval('colrs','rcbgmyrk');
+defval('colrs','rcbgmyrkcbgm');
 linew=[1 1 1 1 1 1 1];
 
 defval('dopooledscens',0);
+defval('dopooledPDFs',0)
 defval('sitesel',1); % scenario selection
 defval('scensel',1); % site selection
 defval('limyrs',[2010 2100]);
 defval('dopanels',[1 2]);
+
+if dopooledPDFs
+    sampsloccomponentset=sampsloccomponents;
+    sampsloccomponents=sampsloccomponents{1};
+end
 
 vars=[];
 fvars=[];
@@ -57,8 +63,21 @@ if dopooledscens
         sub=find(~isnan(u0A(:,jj)));
         varu0A(jj)=var(u0A(sub,jj));
     end
-end
 
+    if dopooledPDFs
+        u0B = [];
+        for PDFsel=1:length(sampsloccomponentset);
+            for scensel2=1:size(sampsloccomponentset{PDFsel},2)
+                u0B=[u0B ; squeeze(sum(sampsloccomponentset{PDFsel}{sitesel,scensel2}(:,subcomp{end},subyears),2))];
+            end
+       end
+
+        for jj=1:size(u0B,2)
+            sub=find(~isnan(u0B(:,jj)));
+            varu0B(jj)=var(u0B(sub,jj));
+        end
+    end
+end
 
 clear hl hp;
 if ismember(1,dopanels)
@@ -81,14 +100,33 @@ if ismember(1,dopanels)
     end
 
     if dopooledscens
-        vcur=[varu0A]/1e6;
-        hl(i+1)=patch([yrs yrs(end:-1:1)],[vcur vlast(end:-1:1)],colrs(i+1)); hold on;
-        vars(end+1,:)=vcur;
+
+        if ((dopooledPDFs==0)||(dopooledPDFs==2))
+            vlast=vcur;
+            vcur=[varu0A]/1e6;
+ %          hl(i+1)=patch([yrs yrs(end:-1:1)],[vcur vlast(end:-1:1)],colrs(i+1)); hold on;
+            hl(i+1)=plot(yrs,vcur,colrs(i+1),'linewidth',2);
+            vars(end+1,:)=vcur;
+            vlast=vcur; i=i+1;
+        end
+        if dopooledPDFs>0
+            vlast=vcur;
+            vcur=[varu0B]/1e6;
+            %hl(i+1)=patch([yrs yrs(end:-1:1)],[vcur vlast(end:-1:1)],colrs(i+1)); hold on;
+            hl(i+1)=plot(yrs,vcur,colrs(i+1),'linewidth',2);
+            vars(end+1,:)=vcur;            
+        end
     end
 
     ylabel('m^2');
     if dopooledscens
-        hlg=legend(hl(end:-1:1),'Scen',labls{end:-1:1},'Location','Northwest');
+        if dopooledPDFs==1
+            hlg=legend(hl(end:-1:1),'Scen+PDF',labls{end:-1:1},'Location','Northwest');
+        elseif dopooledPDFs==2
+            hlg=legend(hl(end:-1:1),'PDF','Scen',labls{end:-1:1},'Location','Northwest');
+        else
+            hlg=legend(hl(end:-1:1),'Scen',labls{end:-1:1},'Location','Northwest');
+        end
     else
         hlg=legend(hl(end:-1:1),labls{end:-1:1},'Location','Northwest');
     end
@@ -104,30 +142,56 @@ if ismember(2,dopanels)
     yrs=[targyears(subyears)];
     vlast=zeros(1,length(yrs));
     clear hl;
-
-    if ~dopooledscens
-        varu0A=var(squeeze(sum(sampsloccomponents{sitesel,scensel}(:,subcomp{end},subyears),2)));
+    if dopooledscens
+        if dopooledPDFs
+            denom=varu0B;
+        else
+            denom=varu0A;
+        end
+    else
+        denom=var(squeeze(sum(sampsloccomponents{sitesel,scensel}(:,subcomp{end},subyears),2)));
     end
+
 
     for i=1:length(labls)		
         u=squeeze(sum(sampsloccomponents{sitesel,scensel}(:,subcomp{i},subyears),2));
-        vcur=[var(u)./varu0A];
+        vcur=[var(u)./denom];
         vcur(vcur<(1e-9*max(vcur)))=0;
         hl(i)=patch([yrs yrs(end:-1:1)],[vcur vlast(end:-1:1)],colrs(i)); hold on;
         vlast=vcur;
         fvars(i,:)=vcur;
     end
     if dopooledscens
-        vcur=[varu0A./varu0A];
-        hl(i+1)=patch([yrs yrs(end:-1:1)],[vcur vlast(end:-1:1)],colrs(i+1)); hold on;
-        fvars(end+1,:)=vcur;
+
+       if ((dopooledPDFs==0)||(dopooledPDFs==2))
+            vlast=vcur;
+            vcur=[varu0A./denom];
+ %          hl(i+1)=patch([yrs yrs(end:-1:1)],[vcur vlast(end:-1:1)],colrs(i+1)); hold on;
+            hl(i+1)=plot(yrs,vcur,colrs(i+1),'linewidth',2);
+            fvars(end+1,:)=vcur;
+            vlast=vcur; i=i+1;
+        end
+        if dopooledPDFs>0
+            vlast=vcur;
+            vcur=[varu0B./denom];
+            %hl(i+1)=patch([yrs yrs(end:-1:1)],[vcur vlast(end:-1:1)],colrs(i+1)); hold on;
+            hl(i+1)=plot(yrs,vcur,colrs(i+1),'linewidth',2);
+            fvars(end+1,:)=vcur;            
+        end
+
 
     end
     ylabel('Fraction of variance');
-    ylim([0 1]);
+    ylim([0 max(fvars(:))]);
     if length(dopanels)==1
         if dopooledscens
-            hlg=legend(hl(end:-1:1),'Scen',labls{end:-1:1},'Location','Northwest');
+            if dopooledPDFs==1
+                hlg=legend(hl(end:-1:1),'Scen+PDF',labls{end:-1:1},'Location','Northwest');
+            elseif dopooledPDFs==2
+                hlg=legend(hl(end:-1:1),'PDF','Scen',labls{end:-1:1},'Location','Northwest');
+            else
+                hlg=legend(hl(end:-1:1),'Scen',labls{end:-1:1},'Location','Northwest');
+            end
         else
             hlg=legend(hl(end:-1:1),labls{end:-1:1},'Location','Northwest');
         end
